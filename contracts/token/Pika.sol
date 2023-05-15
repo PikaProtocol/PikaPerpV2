808279b8e6fbc39fbbf753491b6c5a74f2d1d1e4
ethandev0 <ethandev@protonmail.com>
2023-05-15 17:10:04 +0800

    removed redundant code from token contract

--- contracts/token/Pika.sol
+++ contracts/token/Pika.sol
@@ -1,15 +1,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /** @title Pika
     @notice Pika token contract
  */
 contract Pika is AccessControl, ERC20 {
-    /// @dev The identifier of the role which maintains other roles.
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN");
     /// @dev The identifier of the role which allows accounts to mint tokens.
     bytes32 public constant MINTER_ROLE = keccak256("MINTER");
 
@@ -22,10 +19,6 @@ contract Pika is AccessControl, ERC20 {
     ) ERC20(name, symbol) {
         // We are minting initialSupply number of tokens
         _mint(beneficiary, totalSupply);
-        // Add beneficiary as minter
-        _setupRole(MINTER_ROLE, beneficiary);
-        // Add beneficiary as admin
-        _setupRole(ADMIN_ROLE, beneficiary);
         // Set default admin role: it will be able to grant and revoke any roles
         _setupRole(DEFAULT_ADMIN_ROLE, gov);
     }
@@ -33,12 +26,6 @@ contract Pika is AccessControl, ERC20 {
     /// @dev A modifier which checks that the caller has the minter role.
     modifier onlyMinter() {
         require(hasRole(MINTER_ROLE, msg.sender), "PIKA: only minter");
-        _;
-    }
-
-    /// @dev A modifier which checks that the caller has the admin role.
-    modifier onlyAdmin() {
-        require(hasRole(ADMIN_ROLE, msg.sender), "PIKA: only admin");
         _;
     }
 
--- test/token/Pika.js
+++ test/token/Pika.js
@@ -27,14 +27,6 @@ describe("Pika", function () {
     })
   })
 
-  describe("test mint", async function(){
-    it("mint", async function () {
-      await this.pika.connect(this.owner).mint(this.alice.address, "10000000000000000000000")
-      expect(await this.pika.totalSupply()).to.be.equal("1000010000000000000000000000")
-      expect(await this.pika.balanceOf(this.alice.address)).to.be.equal("10000000000000000000000")
-    })
-  })
-
   describe("test setMinter", async function(){
     it("setMinter", async function () {
       await this.pika.connect(this.owner).grantRole("0xf0887ba65ee2024ea881d91b74c2450ef19e1557f03bed3ea9f16b037cbe2dc9", this.alice.address)
