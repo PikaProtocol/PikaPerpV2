c06fa9ce96f6958b52dc04c3b9d2d54217964863
ethandev0 <ethandev@protonmail.com>
2023-05-24 01:32:50 +0800

    update script

--- scripts/createPika.js
+++ scripts/createPika.js
@@ -5,7 +5,7 @@ async function main() {
     // We get the contract to deploy
     const Pika = await ethers.getContractFactory("Pika");
     // const pika = await Pika.deploy("0x26D6ff77d5D45c91e288bf24540bb232f775020C", "0x26D6ff77d5D45c91e288bf24540bb232f775020C");
-    const pika = await Pika.deploy("Pika", "PIKA", "1000000000000000000000000000", "0x349D5BF05960F02c6d228F5ECFE4793ec1B1130F", "0x349D5BF05960F02c6d228F5ECFE4793ec1B1130F");
+    const pika = await Pika.deploy("Pika", "PIKA", "100000000000000000000000000", "0xc71f84d1f6dfa786B2b7B8B26FCB88120c472e8e", "0xEF4820f560a2294f94e6a019d2964AD61f839f58");
     console.log("Pika deployed to:", pika.address);
 }
 
--- scripts/createPikaTokenGeneration.js
+++ scripts/createPikaTokenGeneration.js
@@ -2,7 +2,11 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
     const PikaTokenGeneration = await ethers.getContractFactory("PikaTokenGeneration");
-    const pikaTokenGeneration = await PikaTokenGeneration.deploy("0x5ED879fA2d41fAadf4054c7b3ED1003ebCE59198", "0x349D5BF05960F02c6d228F5ECFE4793ec1B1130F", "1650949098", "1651035498", "1651121898", "1000000000000000000", "100000000000000000000", "50000000000000000", "100000000000000000", "150000000000000000", "0xd283bb82523d22bdfe2d5f9b74cf1d2c34c1a6dd402976f28d2c8a97d13af551"); // rinkeby
+    const startTime = 1684850400;
+    const pikaTokenGeneration = await PikaTokenGeneration.deploy("0x6c4ed8CCCD7546d5bc30f468b1451a3936489276", "0xc52888bF1beD863F1079a4C84779C99d7bBBA56d",
+        startTime.toString(), (startTime + 1800).toString(), (startTime + 86400).toString(), (startTime + 86400 * 4).toString(),
+        ["1000000000000000000000", "6600000000000000000000"], "19000000000000000000000000", "1000000000000000000", ["1000000000000000000", "3000000000000000000", "5000000000000000000"],
+        "0x90381b977a3a6bde48cbb85ec99ae1c4adf33e605daf0e57f17fc08ccd18fc56"); // rinkeby
     await pikaTokenGeneration.deployed();
     console.log("PikaTokenGeneration deployed to:", pikaTokenGeneration.address);
 }
